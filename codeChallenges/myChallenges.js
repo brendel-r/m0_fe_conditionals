c8b245ffdb94429cdcacb7cd195be3d7c634a7f8
Rachel Brendel <brendelrachel@gmail.com>
2023-01-12 21:56:05 -0600

    update challenge 2

--- codeChallenges/myChallenges.js
+++ codeChallenges/myChallenges.js
@@ -1,20 +1,22 @@
 // challenge 1
-var age = 33;
-age = 14;
+
+// var age = 33;
+// age = 14;
 // age = 37;
-if (age % 2 === 0) {
-    console.log("The number is even.");
-} else {
-    console.log("The number is odd.");
-}
+// if (age % 2 === 0) {
+    // console.log("The number is even.");
+// } else {
+    // console.log("The number is odd.");
+// }
 
 
 // challenge 2
+
 var goodDrivingRecord = true;
 var age = 24;
-//age = 30
- //age = 15
- //goodDrivingRecord = false
+age = 30;
+age = 15;
+goodDrivingRecord = false;
 
 // good driving record is true and age > 25 "you get a discount!"
 // good driving record is true or age is > 25 "Sorry,you pay full price!"
@@ -22,10 +24,8 @@ var age = 24;
 
 if (age > 25 && goodDrivingRecord === true) {
   console.log("You get a discount!!!");
-} else if (goodDrivingRecord === true || age > 25) {
+} else if (goodDrivingRecord === true || age >= 25) {
   console.log("Sorry, you pay full price");
-} else if ( age < 25 && goodDrivingRecord === false) {
+} else {
   console.log("Have an adult sign")
 }
-
-// challenge 3
